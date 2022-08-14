63338b4214b10f8df3b9b56f30d39e6e17b8a294
Parteh the Coder <89151213+PartehDev@users.noreply.github.com>
2022-08-14 15:53:57 -0500

    customize this all you'd like!

---
+++ src/main.py
@@ -0,0 +1,13 @@
+# Customize this all you'd like!
+code = input("> ")
+if code == "help":
+  print("""
+    <Command 1> - <What it does>
+    <Command 2> - <What it does>
+    ...
+    """)
+if code == "<command 1>:
+  # do something
+elif code == "<command 2>:
+  # do something else
+# ...
